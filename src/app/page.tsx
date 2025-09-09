848f55cb6513c10411bf1ff7028a5f04d13c5684
Serie <120567699+RearAlice@users.noreply.github.com>
2025-09-09 19:35:17 +0900

    Add Norton Safe Web site verification meta tag
    Inserted a meta tag for Norton Safe Web site verification in the <head> section to support site validation and security checks.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
       {/* Remove <Head> and use next/head */}
       <head>
         <title>VketReal in Tours - 有志開催のVketRealのポータルサイト- </title>
+        <meta name="norton-safeweb-site-verification" content="1LOU0YUM25-JR4QVHMG9EG10FMX2S0K5GZFCQFH4HM-10-FCWN2BLNNOA3F4WTAZPK7AWYT0OG4KA8I1H0TSNRUQRH6IMY5Y1VL4-P3E28VVFT0WZUMY1AELPJ3WMMEE" />
         <meta name="description" content="VketReal in Toursは、有志開催のVketRealのポータルサイトです。" />
       </head>
       <Header />
